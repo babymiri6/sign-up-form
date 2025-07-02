4a099e57636c35f403be55adb3d7fa106992a6f8
Miriam Ephraim <142228197+babymiri6@users.noreply.github.com>
2025-07-02 04:32:33 +0000

    I added Some JS functions. More on that later

---
+++ script.js
@@ -0,0 +1,43 @@
+//Getting element by Id
+const myForm = document.getElementById("form");
+const message = document.getElementById("bounceMessage")
+let error = "";
+
+//Adding an event listener
+myForm.addEventListener("submit", function(event) {
+
+    event.preventDefault();  //  to stop the form from submitting immediately
+    
+    const password = document.getElementById("password").value;     //.value helps you grab the content of the password
+
+    console.log(password);      //displaying the password to make sure we are fetching the right thing
+
+    validatePassword(password);
+});
+
+function validatePassword(password){
+    if(!/[A-Z]/.test(password)){
+        error = "Password must contain an uppercase";
+        console.log(error);   //this is to make sure the error message has been stored in the error variable well
+    }
+
+    else if(!/[a-z]/.test(password)){
+        error = "Password must contain a lowercase";
+        console.log(error);
+    }
+
+    else if(!/\d/.test(password)){
+        error = "Password must contain at least one number";
+        console.log(error);
+    }
+
+    else if(password.length < 8){
+        error = "Password should be at least 8 characters long";
+        console.log(error);
+    }
+    
+    else{
+        error = "You have successfully signed up";
+        console.log(error);
+    }
+}
