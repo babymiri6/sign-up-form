751754a027533c513f76bcc2a232b47892741e62
Miriam Ephraim <142228197+babymiri6@users.noreply.github.com>
2025-07-03 05:03:14 +0000

    I implemented my first ever password validator for a sign up screen using Javascript

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Getting element by Id
 const myForm = document.getElementById("form");
-const message = document.getElementById("bounceMessage")
-let error = "";
+const message1 = document.getElementById("bounceMessage");
+let error = "the";
 
 //Adding an event listener
 myForm.addEventListener("submit", function(event) {
@@ -12,32 +12,38 @@ myForm.addEventListener("submit", function(event) {
 
     console.log(password);      //displaying the password to make sure we are fetching the right thing
 
+    message1.style.color = "red";
+
     validatePassword(password);
 });
 
 function validatePassword(password){
     if(!/[A-Z]/.test(password)){
-        error = "Password must contain an uppercase";
-        console.log(error);   //this is to make sure the error message has been stored in the error variable well
+        message1.innerText = "Password must contain an uppercase";
+        // console.log(error);   //this is to make sure the error message has been stored in the error variable well
     }
 
     else if(!/[a-z]/.test(password)){
-        error = "Password must contain a lowercase";
-        console.log(error);
+        message1.innerText = "Password must contain a lowercase";
+        // console.log(error);
     }
 
     else if(!/\d/.test(password)){
-        error = "Password must contain at least one number";
-        console.log(error);
+        message1.innerText = "Password must contain at least one number";
+        // console.log(error);
     }
 
     else if(password.length < 8){
-        error = "Password should be at least 8 characters long";
-        console.log(error);
+        message1.innerText = "Password should be at least 8 characters long";
+        // console.log(error);
     }
     
     else{
-        error = "You have successfully signed up";
-        console.log(error);
+        message1.innerText = "Password is stong!";
+        message1.style.color = "green"
+        // console.log(error);
     }
+
 }
+
+ // message.textContent = error;  //This can also be used.
