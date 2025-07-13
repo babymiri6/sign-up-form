79870f0bfea6dca3ee3562c9932b4d02ed5fff65
Miriam Ephraim <142228197+babymiri6@users.noreply.github.com>
2025-07-13 01:37:27 +0000

    I added another html file for the login page and style it accordingly.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Getting element by Id
 const myForm = document.getElementById("form");
-const message1 = document.getElementById("bounceMessage");
+const message = document.getElementById("bounceMessage");
 let error = "the";
 
 //Adding an event listener
@@ -12,35 +12,35 @@ myForm.addEventListener("submit", function(event) {
 
     console.log(password);      //displaying the password to make sure we are fetching the right thing
 
-    message1.style.color = "red";
+    message.style.color = "red";
 
     validatePassword(password);
 });
 
 function validatePassword(password){
     if(!/[A-Z]/.test(password)){
-        message1.innerText = "Password must contain an uppercase";
+        message.innerText = "Password must contain an uppercase";
         // console.log(error);   //this is to make sure the error message has been stored in the error variable well
     }
 
     else if(!/[a-z]/.test(password)){
-        message1.innerText = "Password must contain a lowercase";
+        message.innerText = "Password must contain a lowercase";
         // console.log(error);
     }
 
     else if(!/\d/.test(password)){
-        message1.innerText = "Password must contain at least one number";
+        message.innerText = "Password must contain at least one number";
         // console.log(error);
     }
 
     else if(password.length < 8){
-        message1.innerText = "Password should be at least 8 characters long";
+        message.innerText = "Password should be at least 8 characters long";
         // console.log(error);
     }
     
     else{
-        message1.innerText = "Password is stong!";
-        message1.style.color = "green"
+        message.innerText = "Password is stong!";
+        message.style.color = "green"
         // console.log(error);
     }
 
